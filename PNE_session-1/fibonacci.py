2d549f5dd5547b2a4d6482db438a0d5da8768eda
saravj <saravaliente2@gmail.com>
2019-01-26 11:03:22 +0100

    updated

--- PNE_session-1/fibonacci.py
+++ PNE_session-1/fibonacci.py
@@ -2,18 +2,19 @@
 def fibonacci(n):
     n = int(n)
     if n == 0:
-        fibn = 0
+        print('Counting starts at term 1.')
+        exit(1)
     elif n == 1:
+        fibn = 0
+    elif n == 2:
         fibn = 1
     else:
-        fibn = 0
-        prev = 0
-        for i in range(n):
-            prev = i + prev
-            print(prev)
-        fibn = prev
+        fibn = [0, 1]
+        for i in range(n-2):
+            next = fibn[-2] + fibn[-1]
+            fibn.append(next)
     return fibn
 
 nth_term = input('Please, introduce the number of term to calculate: ')
 
-print('The {}th fibonacci number is {}.'.format(nth_term, fibonacci(nth_term)))
+print('\nTerm number {} of the fibonacci series is {}.'.format(nth_term, fibonacci(nth_term)[-1]))
---
+++ PNE_session-1/venv/fibonacci_sum.py
@@ -0,0 +1,38 @@
+
+# from .fibonacci import fibonacci      why does it not work??
+
+def fibonacci(n):
+    n = int(n)
+    if n == 0:
+        print('Counting starts at term 1.')
+        exit(1)
+    elif n == 1:
+        fibn = 0
+    elif n == 2:
+        fibn = 1
+    else:
+        fibn = [0, 1]
+        for i in range(n-2):
+            next = fibn[-2] + fibn[-1]
+            fibn.append(next)
+    return fibn
+
+not_valid = True
+
+while not_valid:
+    try:
+        nth_term = input('Type in the nth term you want to sum numbers up to: ')
+        nth_term = int(nth_term)
+        not_valid = False
+    except:
+        print('Oops. You must introduce a valid integer... Try again.')
+
+numbers_to_n = fibonacci(nth_term)
+
+total = 0
+
+for i in numbers_to_n:
+    total = total + i
+
+
+print('The result of adding all fibonacci numbers up to term {} is {}.'.format(nth_term, total))

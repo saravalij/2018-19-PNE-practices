096a6f02f11e3afc2315189f505ee168fb03b2b3
saravj <saravaliente2 @gmail.com>
2019-01-29 16:46:51 +0100

    Copied into P0.

---
+++ P0/hello.py
@@ -0,0 +1,10 @@
+print('Hello')
+print('Just testing the pycharm IDE')
+print('testing...2')
+print('testing...3')
+
+print('''Alright, 
+now we are adding more prints
+so we will see if it is working fine''')
+
+

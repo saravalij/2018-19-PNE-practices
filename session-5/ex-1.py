8cfabf648552e25b562fad532e69a285e4051ca1
saravj <saravaliente2 @gmail.com>
2019-02-05 16:40:13 +0100

    Example 1 (counting a's) added

--- session-5/ex-1.py
+++ session-5/ex-1.py
@@ -10,7 +10,7 @@ def count_a(seq):
     return result
 
 # Main program
-s = input('Please enter the sequence: ')
+s = 'AACGTTTACG'
 na = count_a(s)
 print("The number of As is: {}".format(na))
 
@@ -18,10 +18,7 @@ print("The number of As is: {}".format(na))
 tl = len(s)
 
 # Calculate the percentage of As in the sequence
-if tl > 0:
-    perc = round(100.0 * na / tl, 1)
-else:
-    perc = 0
+perc = round(100.0 * na / tl, 1)
 
 print('The total length is: {}'.format(tl))
 print('The percentage of As is {}%'.format(perc))

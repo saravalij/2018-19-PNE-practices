06d659f99e7b38fee89565180d3b0433da8089d7
saravj <saravaliente2 @gmail.com>
2019-02-05 16:41:26 +0100

    Example 1 (counting a's) uploaded for the user to introduce a sequence

---
+++ session-5/ex-1-user.py
@@ -0,0 +1,27 @@
+def count_a(seq):
+    """Counting the number of As in the sequence"""
+
+    result = 0
+    for b in seq:
+        if b == 'A':
+            result += 1
+
+    # Return the result
+    return result
+
+# Main program
+s = input('Please enter the sequence: ')
+na = count_a(s)
+print("The number of As is: {}".format(na))
+
+# Calculate the total sequence length
+tl = len(s)
+
+# Calculate the percentage of As in the sequence
+if tl > 0:
+    perc = round(100.0 * na / tl, 1)
+else:
+    perc = 0
+
+print('The total length is: {}'.format(tl))
+print('The percentage of As is {}%'.format(perc))

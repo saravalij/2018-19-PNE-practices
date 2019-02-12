8ed831e3196ba41c4d816b96bd1f909927743434
saravj <saravaliente2 @gmail.com>
2019-02-12 16:58:39 +0100

    fixed loop (user input, client message)

--- session-7/ex-2-client.py
+++ session-7/ex-2-client.py
@@ -3,14 +3,15 @@
 import socket
 
 # First, create the socket. We will always use this parameters: AF_INET y SOCK_STREAM
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 
 PORT = 8082  # teacher: 8080
 IP = '212.128.253.102'  # teacher: 212.128.253.64
 
 while True:
-    s.connect((IP, PORT))
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     message = input('What do you want to say?: ')
+    s.connect((IP, PORT))
     s.send(str.encode(message))
     msg = s.recv(2048).decode('utf-8')
     print('Message:' )

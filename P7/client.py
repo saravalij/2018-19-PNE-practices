7530f335664218d9ef92f00ef170d8833497c233
saravj <saravaliente2 @gmail.com>
2019-03-20 18:29:50 +0100

    Practice 7 added but not finished.

--- P7/client.py
+++ P7/client.py
@@ -1,13 +1,12 @@
 import http.client
 import termcolor
 import json
-import requests
-import sys
+from P1.Seq import Seq
 
 
-conn = http.client.HTTPConnection('rest.ensembl.org/')
+conn = http.client.HTTPConnection('rest.ensembl.org')
 
-conn.request("GET", "/")
+conn.request("GET", "/sequence/id/ENSG00000165879?content-type=application/json")
 
 r1 = conn.getresponse()
 
@@ -17,6 +16,25 @@ data1 = r1.read().decode("utf-8")
 
 r1.close()
 
-people = json.loads(data1)
+gen_data = json.loads(data1)
 
-print(people)
+seq = gen_data['seq']
+
+seq = Seq(seq)
+
+print('There are {} bases in the FRAT1 gene.'.format(seq.len()))
+
+print('Number of T bases: {}.'.format(seq.count('T')))
+
+bases = ['A','C','G','T']
+great = 0
+
+for b in bases:
+    if seq.count(b) > great:
+        great = seq.count(b)
+        popular = b
+
+print('The most popular base in the sequence is {}, being its percentage {}%.'.format(popular, seq.perc(popular)))
+
+for b in bases:
+    print('Percentage of {} is: {}.'.format(b, seq.perc(b)))
